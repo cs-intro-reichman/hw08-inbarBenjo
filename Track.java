ca374c87f228eb4275a3bf1815d803aaf28efc02
inbarBenjo <154405262+inbarBenjo@users.noreply.github.com>
2024-02-08 15:34:22 +0200

    test1

--- Track.java
+++ Track.java
@@ -41,8 +41,24 @@ class Track {
 
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
-    private String formattedDuration(int totalSeconds) {
+    private String formattedDuration(int totalSeconds) 
+    {
         //// replace the following statement with your code
-        return "";
+       
+        int minutes = totalSeconds / 60 ; 
+        int seconds = totalSeconds % 60; 
+        String secondsStr;
+        if (seconds < 10)
+         {
+            secondsStr = "0" + seconds; 
+         }
+        else
+         {
+          secondsStr = String.valueOf(seconds); 
+         }
+
+        String text = minutes + ":" + secondsStr; 
+
+        return text;
     }
 }
